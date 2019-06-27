a817ee82d1ee24ba4d7da653c58dbda4f801df1f
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-06-27 00:53:36 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,44 @@
+def using_push(arr, str)
+  arr.push(str)
+end
+
+def using_unshift(arr, str)
+  arr.unshift(str)
+end
+
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args(arr)
+  arr.pop(2)
+end
+
+def using_shift(arr)
+  arr.shift
+end
+
+def shift_with_args(arr)
+  arr.shift(2)
+end
+
+def using_concat(arr1, arr2)
+  arr.concat(arr2)
+end
+
+def using_insert(arr, element)
+  arr.insert(element, 4)
+end
+
+def using_uniq(arr)
+  arr.
+def
+
+def using_flatten(arr)
+  arr.flatten
+end
+
+def using_delete(arr, str)
+  arr.delete(str)
+end
+
